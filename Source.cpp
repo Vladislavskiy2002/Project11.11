ce1e3c737305dedd780752efe24dcd1130fe5823
Volodymir Konovalov <vvkon@meta.ua>
2019-12-27 10:19:23 +0200

    the split function accepts a string (const string & s) and a word that means a space character (const string & w). Compares the word (const string & w) with the word string (const string & s), replaces the word string (const string & s) that is identical to the word (const string & w). Adds a string to the vector vector <string> temp_vec_str. Returns the vector <string> temp_vec_str

--- Source.cpp
+++ Source.cpp
@@ -2,14 +2,12 @@
 #include <locale>
 #include "windows.h"
 
-vector<string> split(const string& s);
-
+vector<string> split(const string& s, const string& w);
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
 	string str = "";
 	string s = "";
 	string w = "(probel)";
@@ -18,20 +16,12 @@ int main()
 		stringstream ss{ s };
 		for (string temp_s; ss >> temp_s;)
 		{
-			if (temp_s == w)
-			{
-				temp_s = "\n";
-				str += temp_s;
-			}
-			else
-				str += temp_s + " ";
+			str += temp_s + " ";
 		}
 		str += '\n';
 	}
 	vector<string> vec_str;
-
-
-	vec_str = split(str);
+	vec_str = split(str, w);
 	for (int i = 0; i < vec_str.size(); i++)
 	{
 		cout << vec_str[i] << endl;
@@ -42,17 +32,28 @@ int main()
 	return 0;
 }
 
-vector<string> split(const string& s)
+vector<string> split(const string& s, const string& w)
 {
 	vector <string> temp_vec_str;
+	string str;
 	stringstream ss{ s };
-	string st;
-	for (string temp_s; getline(ss, temp_s);)
+	for (string temp_str_get; getline(ss,temp_str_get);)
 	{
-		st += temp_s;
-		temp_vec_str.push_back(st);
-		st = "";
-
+		stringstream sw{ temp_str_get };
+		for (string temp_s; sw >> temp_s;)
+		{
+			if (temp_s == w || temp_s == "\n")
+			{
+				str += "\n";
+				continue;
+			}
+			else
+			{
+				str += temp_s + " ";
+			}
+		}
+		temp_vec_str.push_back(str);
+		str = "";
 	}
 	return temp_vec_str;
 }
